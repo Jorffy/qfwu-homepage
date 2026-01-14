cd4d076b046a4a2b0a4bbf43bc51fe2a3464b023
SCNU_WQF <65220662+Jorffy@users.noreply.github.com>
2026-01-15 02:02:11 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -2,20 +2,40 @@ from scholarly import scholarly, ProxyGenerator
 import jsonpickle
 import json
 from datetime import datetime
-import os
+import os, time
 from scholarly._proxy_generator import MaxTriesExceededException
 
+max_attempts = 100
+wait_seconds = 10  # 10 minutes
 
-try:
-    print("正在查找作者信息...")
-    # Setup proxy
-    pg = ProxyGenerator()
-    pg.FreeProxies()  # Use free rotating proxies
-    scholarly.use_proxy(pg)
-    # author: dict = scholarly.search_author_id(os.environ["GOOGLE_SCHOLAR_ID"])
-    author: dict = scholarly.search_author_id("zo8SfrMAAAAJ")
-except MaxTriesExceededException as e:
-    print(f"发生异常: {e}")
+for attempt in range(1, max_attempts + 1):
+    try:
+        print(f"Attempt {attempt}:")
+        # Setup proxy
+        pg = ProxyGenerator()
+        pg.FreeProxies()  # Use free rotating proxies
+        scholarly.use_proxy(pg)
+        
+        author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+        # scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+        print(f"Attempt {attempt} success")
+        break  # Exit loop on first success
+    except Exception as e:
+        print(f"Attempt {attempt} failed with error: {e}")
+        time.sleep(wait_seconds)
+else:
+    print("All 100 attempts failed.")
+
+# try:
+#     print("正在查找作者信息...")
+#     # Setup proxy
+#     pg = ProxyGenerator()
+#     pg.FreeProxies()  # Use free rotating proxies
+#     scholarly.use_proxy(pg)
+#     # author: dict = scholarly.search_author_id(os.environ["GOOGLE_SCHOLAR_ID"])
+#     author: dict = scholarly.search_author_id("zo8SfrMAAAAJ")
+# except MaxTriesExceededException as e:
+#     print(f"发生异常: {e}")
 
 print("正在填充作者详细信息...")
 scholarly.fill(author, sections=["basics", "indices", "counts", "publications"])
